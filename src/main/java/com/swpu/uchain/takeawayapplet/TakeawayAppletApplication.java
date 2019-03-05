170c60bbf7c0f64a1778358c95c0b4ee2e821121
TreasureJade <1056024860@qq.com>
2019-03-05 20:56:10 +0800

    git basis

--- src/main/java/com/swpu/uchain/takeawayapplet/TakeawayAppletApplication.java
+++ src/main/java/com/swpu/uchain/takeawayapplet/TakeawayAppletApplication.java
@@ -1,9 +1,11 @@
 package com.swpu.uchain.takeawayapplet;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.swpu.uchain.takeawayapplet.dao")
 public class TakeawayAppletApplication {
 
     public static void main(String[] args) {
